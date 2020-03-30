d882ac9687436e9b811fc238046422a4f62d95e2
vinay <vinay@LAPTOP-CVR9HVBP>
2020-03-30 10:55:38 +0530

    cts project

---
+++ src/test/java/TESTCASES/Test2.java
@@ -0,0 +1,94 @@
+package TESTCASES;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import libary.Utility;
+import pages.Add_Task_Page;
+import pages.Login_page;
+import pages.Search_page;
+
+public class Test2 extends Utility {
+	Add_Task_Page ad;
+	Login_page l;
+	Search_page s;
+	int num=2;
+	 @BeforeMethod
+	  public void lanuchBrowser() {
+		  d=launchBroser("FIREFOX", "http://examples.codecharge.com/TaskManager/Default.php");//Launch of Browser with Chrome
+	  }
+	
+	@Test(dataProvider="valid_login_data",priority=0)
+	public void login_valid(String userName,String password,String expected_result)
+	{
+		l=new Login_page(d);
+		  l.do_login(userName,password);//Login with Valid data
+		  String actual_result=l.log();//Get result of login
+		  l.ck_task();//click on search 
+		  s=new Search_page(d);
+		  s.do_serch("CodeCharge", "High", "Closed", "Task", "Peter Larsen");//search task 
+		  ScreenShot();//take screenshot
+		  Assert.assertTrue(actual_result.contains(expected_result));//compare result
+		  
+
+	}
+	@DataProvider
+	  public String[][] valid_login_data() {
+		  get_data("valid",1,3);//Get data from login excel
+		 return data;
+	   
+	  }
+	@Test(dataProvider="invalid_login_data",priority=1)
+	public void login_invalid(String userName,String password,String expected_message)
+	{
+		l=new Login_page(d);
+		  l.do_login(userName,password);//Login with invalid data
+		  String actual_message=l.error();//Get result of login
+		  ScreenShot();//take screenshot
+		  Assert.assertTrue(actual_message.contains(expected_message));//compare result
+		  
+
+	}
+	@DataProvider
+	  public String[][] invalid_login_data() {
+		  get_data("invalid",3,3);//Get data from login excel
+		 return data;
+	   
+	  }
+	@Test(dataProvider="blank_login_data",priority=2)
+	public void login_blank(String userName,String password,String expected_message)
+	{
+		l=new Login_page(d);
+		  l.do_login(userName,password);//Login with blank data
+		  String actual_message=l.error();//Get result of login
+		  System.out.println(actual_message);
+		  ScreenShot();//take screenshot
+		  Assert.assertTrue(actual_message.contains(expected_message));//compare result
+		  
+
+	}
+	@DataProvider
+	  public String[][] blank_login_data() {
+		  get_data("blank",2,3);//Get data from blank excel
+		 return data;
+	   
+	  }
+
+  @AfterMethod
+  public void closeBrowser() {
+	  d.close();//close Browser
+	  
+	  
+	  
+  }
+
+
+
+  
+ 
+
+}
+
--- src/test/java/pages/Login_page.java
+++ src/test/java/pages/Login_page.java
@@ -5,23 +5,29 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import libary.utility;
+import libary.Utility;
 
-public class login {
+
+public class Login_page {
 	WebDriver d;
-	public login(WebDriver d)
+	public Login_page(WebDriver d)
 	{
 		this.d=d;
 	}
-	utility u=new utility();
+	Utility u=new Utility();
 	By admin=By.xpath("//a[@href='Administration.php']");
 	By user=By.xpath("//input[@name='login']");
 	By pass=By.xpath("//input[@name='password']");
 	By login=By.xpath("//input[@name='DoLogin']");
 	By logout=By.xpath("//a[@href='Login.php?Logout=True']");
+	By task_btn=By.xpath("//a[@href='Default.php']");
 	By error=By.xpath("//tr[@class='Error']");
 	public void ck_ad() {
 		WebElement we=u.elementclick(d,admin, 20);
+		we.click();
+	}
+	public void ck_task() {
+		WebElement we=u.elementclick(d,task_btn, 20);
 		we.click();
 	}
 	public void se_lo(String a) {
@@ -50,39 +56,29 @@ public class login {
 		WebElement we=u.elementclick(d,login, 20);
 		we.click();
 	}
-	public String lg()
+	public String log()
 	{
 		WebElement we=u.waitForElement(d,logout, 20);
 		return we.getText();
 		
 	}
-	public String er()
+	public String error()
 	{
 		WebElement we=u.waitForElement(d,error, 20);
 		return we.getText();
 		
 	}
-	public void do_login(String u,String p)
+	public void do_login(String user,String pass)
 	{
 		this.ck_ad();
-		this.se_lo(u);
-		this.se_ps(p);
+		this.se_lo(user);
+		this.se_ps(pass);
 		this.ck_bt();
 	}
-	public String error_check()
-	{
-		 try {
-			 return this.lg();
-
-		  }  catch(NoSuchElementException e) {
-			  return this.er();
-		  }
-		 catch(NullPointerException e)
-		 {
-			  return this.er();
-		 }
+	
+	
 		  
-	}
+	
 	
 
 }
